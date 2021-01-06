7e92b64371023af94390a213f9ca549a76d71ad3
czx <2062161343@qq.com>
2021-01-06 21:33:08 +0800

    1.6.3

---
+++ js/common.js
@@ -0,0 +1,24 @@
+window.onload = function() {
+
+    // 页面滚动顶部导航栏固定事件
+    window.onscroll = scroll;
+
+
+
+
+
+
+
+    // 页面滚动顶部导航栏固定
+    function scroll() {
+        if ($(window).scrollTop() > $('#header').outerHeight(true)) {
+            $('#fixhead').css({
+                display: 'block'
+            });
+        } else {
+            $('#fixhead').css({
+                display: 'none'
+            });
+        }
+    }
+}
